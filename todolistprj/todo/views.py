3a81df1291f9879cbe9b469ee9a616aabef6284c
Leah Grim <grimchelate@gmail.com>
2023-05-22 10:36:37 -0500

    created the task object in views.py

--- todolistprj/todo/views.py
+++ todolistprj/todo/views.py
@@ -5,5 +5,6 @@ from .models import Task
 
 class TaskList(ListView):
     model = Task 
-
+    # rename context object name to use task in the html in the for loop
+    context_object_name = 'task' 
 
