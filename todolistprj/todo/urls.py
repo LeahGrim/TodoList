98a3ed63064d2afe4a32b1a9a91179eb47184704
Leah Grim <grimchelate@gmail.com>
2023-05-22 11:19:27 -0500

    created urls and model views for task form.html

--- todolistprj/todo/urls.py
+++ todolistprj/todo/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import TaskList, TaskDetail
+from .views import TaskList, TaskDetail, TaskCreate
 
 
 urlpatterns = [
     path('', TaskList.as_view(), name="task"),
+    path ('task-create/', TaskCreate.as_view(), name="task-create"), 
     path('task/<int:pk>/', TaskDetail.as_view(), name="tasks-detail")
 ]
--- todolistprj/todo/views.py
+++ todolistprj/todo/views.py
@@ -2,6 +2,9 @@ from django.shortcuts import render
 from django.http import HttpResponse 
 from django.views.generic.list import ListView
 from django.views.generic.detail import DetailView
+from django.views.generic.edit import CreateView 
+
+from django.urls import reverse_lazy
 from .models import Task
 
 class TaskList(ListView):
@@ -13,3 +16,11 @@ class TaskDetail(DetailView):
     model = Task 
     context_object_name = 'task'
     template_name = 'todo/task.html'
+
+
+class TaskCreate(CreateView):
+    model = Task
+    # this will show all the fields that we have created in models.py
+    fields = "__all__"
+    # after creating new task, user will be brought to new page
+    success_url = reverse_lazy('task')
