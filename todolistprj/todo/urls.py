02f0bf1a88fe7cad1fd5d8fa699878da351b8e63
Leah Grim <grimchelate@gmail.com>
2023-05-22 14:03:20 -0500

    added login information

--- todolistprj/todo/urls.py
+++ todolistprj/todo/urls.py
@@ -1,11 +1,13 @@
 from django.urls import path
-from .views import TaskList, TaskDetail, TaskCreate, TaskUpdate, TaskDelete
+from .views import TaskList, TaskDetail, TaskCreate, TaskUpdate, TaskDelete, CustomLoginView
 
 
 urlpatterns = [
     path('', TaskList.as_view(), name="task"),
     path ('task-create/', TaskCreate.as_view(), name="task-create"), 
+    path('login/', CustomLoginView.as_view(), name="login"),
     path('task/<int:pk>/', TaskDetail.as_view(), name="tasks-detail"),
     path('task-update/<int:pk>/', TaskUpdate.as_view(), name="tasks-update"),
-    path('task-delete/<int:pk>/', TaskDelete.as_view(), name="tasks-delete")
+    path('task-delete/<int:pk>/', TaskDelete.as_view(), name="tasks-delete"),
+    
 ]
--- todolistprj/todo/views.py
+++ todolistprj/todo/views.py
@@ -3,6 +3,7 @@ from django.http import HttpResponse
 from django.views.generic.list import ListView
 from django.views.generic.detail import DetailView
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from django.contrib.auth.views import LoginView
 
 from django.urls import reverse_lazy
 from .models import Task
@@ -36,3 +37,10 @@ class TaskDelete(DeleteView):
     context_object_name= 'task'
     success_url = reverse_lazy('task')
     
+class CustomLoginView(LoginView):
+    template_name = 'todo/login.html'
+    fields = "__all__"
+    redirect_authenticated_user = False 
+
+    def get_success_url(self): 
+        return reverse_lazy('task')
