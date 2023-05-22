a2a395821f136f34be09529fec5d998799f0f33c
Leah Grim <grimchelate@gmail.com>
2023-05-22 09:45:53 -0500

    created user, title, and description models in models.py

--- todolistprj/todo/models.py
+++ todolistprj/todo/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+# import user
+from django.contrib.auth.models import User 
 
 # Create your models here.
+class Task(models.Model):
+    # create a foreign key model to manage everything related to user in our app
+    # if our user is deleted, data is deleted 
+    user= models.ForeignKey(User, on_delete= models.CASCADE, null= True, blank=True)
+    title =  models.CharField(max_length=200, null=True)
+    # TextField automatically alerts django that there will be lots of text, no need to specify max length
+    description = models.TextField(null=True, blank=True)
