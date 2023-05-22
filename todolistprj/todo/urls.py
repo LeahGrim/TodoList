9c204604c9ff4903ca73c23aeab1231673a4d018
Leah Grim <grimchelate@gmail.com>
2023-05-22 14:14:51 -0500

    added a logout url to urls.py

--- todolistprj/todo/urls.py
+++ todolistprj/todo/urls.py
@@ -1,13 +1,14 @@
 from django.urls import path
 from .views import TaskList, TaskDetail, TaskCreate, TaskUpdate, TaskDelete, CustomLoginView
-
+from django.contrib.auth.views import LogoutView
 
 urlpatterns = [
     path('', TaskList.as_view(), name="task"),
     path ('task-create/', TaskCreate.as_view(), name="task-create"), 
     path('login/', CustomLoginView.as_view(), name="login"),
+    path('logout/', LogoutView.as_view(), name="logout"),
     path('task/<int:pk>/', TaskDetail.as_view(), name="tasks-detail"),
     path('task-update/<int:pk>/', TaskUpdate.as_view(), name="tasks-update"),
     path('task-delete/<int:pk>/', TaskDelete.as_view(), name="tasks-delete"),
-    
+
 ]
