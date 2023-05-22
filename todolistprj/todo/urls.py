510e1c0125c6047b148cc064f284ee5da0cdd49b
Leah Grim <grimchelate@gmail.com>
2023-05-22 14:56:34 -0500

    added register url to urls.py fixed views.py

--- todolistprj/todo/urls.py
+++ todolistprj/todo/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import TaskList, TaskDetail, TaskCreate, TaskUpdate, TaskDelete, CustomLoginView
+from .views import TaskList, TaskDetail, TaskCreate, TaskUpdate, TaskDelete, CustomLoginView, RegisterPage
 from django.contrib.auth.views import LogoutView
 
 urlpatterns = [
@@ -7,7 +7,7 @@ urlpatterns = [
     path ('task-create/', TaskCreate.as_view(), name="task-create"), 
     path('login/', CustomLoginView.as_view(), name="login"),
     path('logout/', LogoutView.as_view(next_page='login'), name="logout"),
-    path('register/', LogoutView.as_view(next_page='login'), name="logout"),
+    path('register/', RegisterPage.as_view(), name="register"),
     path('task/<int:pk>/', TaskDetail.as_view(), name="tasks-detail"),
     path('task-update/<int:pk>/', TaskUpdate.as_view(), name="tasks-update"),
     path('task-delete/<int:pk>/', TaskDelete.as_view(), name="tasks-delete"),
--- todolistprj/todo/views.py
+++ todolistprj/todo/views.py
@@ -4,8 +4,11 @@ from django.views.generic.list import ListView
 from django.views.generic.detail import DetailView
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from django.contrib.auth.views import LoginView
-from django.contrib.auth.mixins import LoginRequiredMixin
+from django.views.generic.edit import FormView
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth import login
 
+from django.contrib.auth.mixins import LoginRequiredMixin
 from django.urls import reverse_lazy
 from .models import Task
 
@@ -56,3 +59,10 @@ class CustomLoginView(LoginView):
 
     def get_success_url(self): 
         return reverse_lazy('task')
+
+
+class RegisterPage(FormView):
+    template_name = 'todo/register.html'
+    form_class = UserCreationForm
+    redirect_authenticated_user= True
+    success_url = reverse_lazy('task')
