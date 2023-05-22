63fee1a5b8bfd8ec32843e6b6c25be266cd7e2e9
Leah Grim <grimchelate@gmail.com>
2023-05-22 15:04:27 -0500

    user is redirected to task list html after registration

--- todolistprj/todo/views.py
+++ todolistprj/todo/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse 
 from django.views.generic.list import ListView
 from django.views.generic.detail import DetailView
@@ -73,3 +73,8 @@ class RegisterPage(FormView):
         if user is not None: 
             login(self.request, user)
         return super(RegisterPage, self).form_valid(form)
+
+    def get(self, *args, **kwargs):
+        if self.request.user.is_authenticated: 
+            return redirect('task')
+        return super(RegisterPage, self).get(*args, **kwargs)
