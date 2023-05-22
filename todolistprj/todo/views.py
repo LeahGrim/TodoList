9b3c44a7dee4233398f1bee1e1ad9bff0417cefe
Leah Grim <grimchelate@gmail.com>
2023-05-22 10:22:05 -0500

    added TaskList to views.py

--- todolistprj/todo/views.py
+++ todolistprj/todo/views.py
@@ -1,4 +1,9 @@
 from django.shortcuts import render
 from django.http import HttpResponse 
-# Create your views here.
+from django.views.generic.list import ListView
+from .models import Task
+
+class TaskList(ListView):
+    model = Task 
+
 
