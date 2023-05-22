803db3708cbdb2d6edab2ded46ddcf987751cb01
Leah Grim <grimchelate@gmail.com>
2023-05-22 09:49:17 -0500

    added complete and created models in models.py

--- todolistprj/todo/models.py
+++ todolistprj/todo/models.py
@@ -7,6 +7,10 @@ class Task(models.Model):
     # create a foreign key model to manage everything related to user in our app
     # if our user is deleted, data is deleted 
     user= models.ForeignKey(User, on_delete= models.CASCADE, null= True, blank=True)
-    title =  models.CharField(max_length=200, null=True)
+    title = models.CharField(max_length=200, null=True)
     # TextField automatically alerts django that there will be lots of text, no need to specify max length
     description = models.TextField(null=True, blank=True)
+    # creates the checklist with an ability to complete the task created, when it is completed user will switch it to True
+    complete = models.BooleanField(default=False)
+    #when a new task is created, the date and time will automatically be assigned to the task
+    created = models.DateTimeField(auto_now_add=True)
